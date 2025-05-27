496bf43499bd6afad03d9d448a1b9c84f2f41b86
Bruno Peres Christino <brunowebdeveloper33@gmail.com>
2025-05-27 17:44:19 +0300

    fix: build page error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/knights-tour', // Replace with your repo name
+};
 
 export default nextConfig;
