966f08eef944b97656de5514d221db810d1ce201
Bruno Peres <brunobpc.fab@gmail.com>
2025-06-21 13:24:54 +0300

    add deploy workflow

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   output: 'export',
-  basePath: isProd ? '/knights-tour' : '',
-  assetPrefix: isProd ? '/knights-tour/' : '',
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
 };
